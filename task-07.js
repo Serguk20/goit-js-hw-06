e78c457c729a7db12b01f3c0ae28a0f8243b73a4
Sergiy <sergnarik@yahoo.com>
2023-08-24 00:04:21 +0100

    markup

--- task-07.js
+++
@@ -1,9 +0,0 @@
-
-const fontSizeControl = document.querySelector("#font-size-control");
-const text = document.querySelector("#text");
-
-fontSizeControl.addEventListener("input", function() {
-  text.style.fontSize = `${fontSizeControl.value}px`;
-});
-
-console.log(fontSizeControl);
